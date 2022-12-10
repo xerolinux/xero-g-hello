437629e277358898fefcdd6b7decd619dd412e39
TechXero <steve@techxero.com>
2022-12-10 15:49:07 +0200

    Scripts

--- src/scripts/apply_defaults.sh
+++ src/scripts/apply_defaults.sh
@@ -11,14 +11,14 @@ echo "#                         !!! XeroLinux Reset Tool !!!                    
 echo "#                                                                             #"
 echo "#              Having Issues With Messed Up Layout or Settings ?              #"
 echo "#                                                                             #"
-echo "# This Will Apply Stock Defaults. Layout WILL BE RESET, Your Settings UNDONE! #"
+echo "#            This Will Restore Stock Defaults. Layout WILL BE RESET           #"
 echo "###############################################################################"
 tput sgr0
 echo
 echo "Please Select Which Defaults you want to apply..."
 echo
-echo "1.  Apply Latest Defaults. (Current)"
-echo "2.  Apply Previous Defaults. (Latte + KDE Panel)"
+echo "1.  Restore KDE Defaults."
+echo "2.  Restore Gnome Defaults."
 echo
 echo "3.  Exit"
 echo
@@ -36,10 +36,9 @@ case $CHOICE in
       rm -Rf ~/.local/share/plasma/
       sleep 2
       echo "###################################"
-      echo "Applying latest XeroLinux defaults "
+      echo "       Restoring KDE defaults      "
       echo "###################################"
       sleep 2
-      sudo pacman -Rdd kde-rice --noconfirm
       sudo pacman -Rdd kvantum --noconfirm
       sudo pacman -S xero-kde-config lightly-git latte-dock-git --noconfirm --needed
       cp -rf /etc/skel/. ~
@@ -48,11 +47,10 @@ case $CHOICE in
       cd ~/default-grub/ && sudo ./install.sh
       rm -rf ~/default-grub
       sleep 2
-      echo "#################################"
-      echo "Done! Rebooting to Apply Settings"
-      echo "#################################"
-      sleep 2
-      reboot
+      echo "##################################"
+      echo "  Done! Reboot to Apply Settings  "
+      echo "##################################"
+      sleep 6
 
       ;;
 
@@ -60,26 +58,22 @@ case $CHOICE in
       echo "Creating Backups of ~/.config folder"
       echo "#####################################"
       cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
-      rm -Rf ~/.local/share/plasma/
       sleep 2
-      echo "#################################"
-      echo "Restoring default settings"
-      echo "#################################"
+      echo "###################################"
+      echo "      Restoring Gnome defaults     "
+      echo "###################################"
       sleep 2
-      sudo pacman -Rdd xero-kde-config --noconfirm
-      sudo pacman -Rdd lightly-git --noconfirm
-      sudo pacman -S kde-rice kvantum kvantum-theme-layan-git --noconfirm --needed
       cp -rf /etc/skel/. ~
-      sudo sed -i "s/Current=.*/Current=XeroDark/" /etc/sddm.conf.d/kde_settings.conf
       cd ~ && git clone https://github.com/xerolinux/default-grub.git
       cd ~/default-grub/ && sudo ./install.sh
       rm -rf ~/default-grub
+      dconf load /org/gnome/ < /etc/skel/.config/xero-dconf.conf &
+      notify-send "XeroG settings applied Please reboot! 🔥"
       sleep 2
-      echo "#################################"
-      echo "Done! Rebooting to Apply Settings"
-      echo "#################################"
-      sleep 2
-      reboot
+      echo "##################################"
+      echo "  Done! Reboot to Apply Settings  "
+      echo "##################################"
+      sleep 6
 
       ;;
 
--- src/scripts/enable_wayland.sh
+++ src/scripts/enable_wayland.sh
@@ -1,14 +1,72 @@
-#!/usr/bin/bash
+#!/bin/bash
 #set -e
-echo "###################################"
-echo "# Adding/Enabling Wayland Support #"
-echo "###################################"
-sleep 3
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	DarkXero
+# Website 	: 	http://xerolinux.xyz
+##################################################################################################################
+echo
+tput setaf 3
+echo "###############################################################################"
+echo "#                         XeroLinux Wayland Enabler                           #"
+echo "#                                                                             #"
+echo "#                            !!!! ATTENTION !!!                               #"
+echo "#                                                                             #"
+echo "#             This Will Allow You To Enable Wayland On Your System            #"
+echo "# Be careful though, if you are on nVidia GPU, this might end up not working. #"
+echo "#                                                                             #"
+echo "#     Enable it AT YOUR OWN RISK ! I will NOT provide any support for it.     #"
+echo "###############################################################################"
+tput sgr0
+echo
+echo "Select which Edition you on."
+echo
+echo "########## Edition Selection ##########"
+echo
+echo "1.  XeroLinux KDE Plasma."
+echo "2.  XeroLinux GNOME (XeroG)."
+echo
+echo "Type Your Selection. To Exit, just close Window."
+echo
 
-sudo pacman -S --noconfirm --needed xorg-xwayland kwayland-integration plasma-wayland-session plasma-wayland-protocols qt6-wayland
+while :; do
 
-sleep 3
-echo "#################################"
-echo "#      Done! Please Reboot      #"
-echo "#################################"
-sleep 6
+read CHOICE
+
+case $CHOICE in
+
+    1 )
+      echo
+      echo "###########################################"
+      echo "    Adding/Enabling KDE Wayland Support    "
+      echo "###########################################"
+			sleep 3
+			sudo pacman -S --noconfirm --needed xorg-xwayland kwayland-integration plasma-wayland-session plasma-wayland-protocols qt6-wayland
+			sleep 3
+      echo "###########################################"
+      echo "  Please Reboot & Select Wayland on Login  "
+      echo "###########################################"
+
+      ;;
+
+    2 )
+      echo
+      echo "###########################################"
+      echo "   Adding/Enabling GNOME Wayland Support   "
+      echo "###########################################"
+			sleep 3
+			sudo sed -i 's|false|true|g' /etc/gdm/custom.conf
+			sleep 3
+      echo "###########################################"
+      echo "  Please Reboot & Select Wayland on Login  "
+      echo "###########################################"
+
+      ;;
+
+    * )
+      echo "#################################"
+      echo "    Choose the correct number    "
+      echo "#################################"
+      ;;
+esac
+done
